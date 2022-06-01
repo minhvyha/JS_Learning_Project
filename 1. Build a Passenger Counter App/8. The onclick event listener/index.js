930d85f5acbb3ae7aa74bffc54b01a0e733738ef
Minh Vy Ha <vyha0512@gmail.com>
2022-06-01 16:24:55 +1000

    lesson 9

--- 1. Build a Passenger Counter App/8. The onclick event listener/index.js
+++ 1. Build a Passenger Counter App/8. The onclick event listener/index.js
@@ -8,7 +8,10 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
-
+function increment() {
+    console.log("The button was clicked")
+    
+}
 
 
 
