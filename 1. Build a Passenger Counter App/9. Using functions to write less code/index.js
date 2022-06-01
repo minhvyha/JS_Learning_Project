0d0c8c0cb37777cf171dd6cdf786b1e0365b97a5
Minh Vy Ha <vyha0512@gmail.com>
2022-06-01 16:26:20 +1000

    lesson 9

--- 1. Build a Passenger Counter App/9. Using functions to write less code/index.js
+++ 1. Build a Passenger Counter App/9. Using functions to write less code/index.js
@@ -1,19 +1,16 @@
 // Setting up the the race 🏎 🏎 🏎
 
-console.log(5)
-console.log(4)
-console.log(3)
-console.log(2)
-console.log(1)
-
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
 // GO! 🏁
 // Players are running the race 🏎 💨
 // Race is finished! 🍾
 
 // Get ready for a new race 🏎 🏎 🏎
+countdown()
 
-console.log(5)
-console.log(4)
-console.log(3)
-console.log(2)
-console.log(1)
