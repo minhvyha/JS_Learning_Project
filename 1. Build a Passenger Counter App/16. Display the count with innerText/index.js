28320033ad328eeb90966e41a963a8ab6653ee83
Minh Vy Ha <vyha0512@gmail.com>
2022-06-03 21:56:18 +1000

    update

--- 1. Build a Passenger Counter App/16. Display the count with innerText/index.js
+++ 1. Build a Passenger Counter App/16. Display the count with innerText/index.js
@@ -1,9 +1,11 @@
 // grab the count-el element, store it in a countEl variable
 let count = 0
+let countEl = document.getElementById('count-el')
 
 function increment() {
     count = count + 1
     // set countEl's innerText to the count
+    countEl.innerHTML = count
 }
 
 
