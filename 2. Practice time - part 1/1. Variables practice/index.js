d5ceee8e654aea48aea8e5d6e845c95e79d90695
Minh Vy Ha <vyha0512@gmail.com>
2022-06-07 20:54:36 +1000

    07/06

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,8 @@
 // Create two variables, firstName and lastName
-
+let firstName = 'Minh Vy'
+let lastName = 'Ha'
 // Concatenate the two variables into a third variable called fullName
-
+let fullName = firstName + ' ' + lastName
 // Log fullName to the console
-
+console.log(fullName)
 
