b22aa96a5d84d2c9373bd9114618c03e40e95ae4
Minh Vy Ha <vyha0512@gmail.com>
2022-06-09 21:01:49 +1000

    09/06

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,3 +2,12 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+if (sum < 21){
+    console.log('Do you want to draw a new card?')
+}
+else if (sum === 21){
+    console.log('Blackjack')
+}
+else{
+    console.log('You are out of the game')
+}
