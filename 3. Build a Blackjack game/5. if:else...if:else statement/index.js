de5247efa022e1a82cd25f9e123fe129cb8061f2
Minh Vy Ha <vyha0512@gmail.com>
2022-06-11 20:48:21 +1000

    11/06

--- 3. Build a Blackjack game/5. if:else...if:else statement/index.js
+++ 3. Build a Blackjack game/5. if:else...if:else statement/index.js
@@ -5,3 +5,12 @@ let age = 100
 // if less than 100    -> "Not elegible"
 // else if exactly 100 -> "Here is your birthday card from the King!"
 // else                -> "Not elegible, you have already gotten one"
+if (age < 100){
+    console.log('Not elegible')
+}
+else if (age === 100){
+    console.log("Here is your birthday card from the King!")
+}
+else{
+    console.log("Not elegible, you have already gotten one")
+}
