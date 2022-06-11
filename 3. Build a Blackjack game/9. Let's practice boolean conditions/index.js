88bc7d40dab304e15b28d18914bf597310a33c4b
Minh Vy Ha <vyha0512@gmail.com>
2022-06-11 20:52:54 +1000

    11/06

--- 3. Build a Blackjack game/9. Let's practice boolean conditions/index.js
+++ 3. Build a Blackjack game/9. Let's practice boolean conditions/index.js
@@ -1,10 +1,10 @@
-console.log(4 === 3)  // 
-console.log(5 > 2)    // 
-console.log(12 > 12)  //
-console.log(3 < 0)    //
-console.log(3 >= 3)   // 
-console.log(11 <= 11) //
-console.log(3 <= 2)   //
+console.log(4 === 3)  // False
+console.log(5 > 2)    // True
+console.log(12 > 12)  //False
+console.log(3 < 0)    //False
+console.log(3 >= 3)   // True
+console.log(11 <= 11) //True
+console.log(3 <= 2)   //False
 
 // let firstCard = 10
 // let secondCard = 11
