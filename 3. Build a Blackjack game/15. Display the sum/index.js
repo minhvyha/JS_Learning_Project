bd493d4756a3ef4385fd519501d8980c3b0c44f0
Minh Vy Ha <vyha0512@gmail.com>
2022-06-16 21:49:10 +1000

    16/06

--- 3. Build a Blackjack game/15. Display the sum/index.js
+++ 3. Build a Blackjack game/15. Display the sum/index.js
@@ -6,6 +6,8 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 // 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById('sum-el')
+
 
 function startGame() {
     // 3. Render the sum on the page using this format -> "Sum: 14"
@@ -19,4 +21,5 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message
+    sumEl.textContent = 'Sum: ' + sum
 }
