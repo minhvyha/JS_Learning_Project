51589a2944e72f7120c0a764389da1bfa4ec3d1a
Minh Vy Ha <vyha0512@gmail.com>
2022-06-17 20:00:01 +1000

    17/06

--- 3. Build a Blackjack game/21. Intro to arrays/index.js
+++ 3. Build a Blackjack game/21. Intro to arrays/index.js
@@ -8,4 +8,7 @@ let featuredPosts = [
 
 // Create an array that lists your i.e. experience, education, licenses, skills or similar
 // The items of the array should be strings
-
+let experience = [
+    'Cabramatta High School',
+    'Pho Ann restaurant'
+]
