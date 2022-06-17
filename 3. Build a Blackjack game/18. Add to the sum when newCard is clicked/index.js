e9231d087e034e438a8d0fcc5543b44640681e92
Minh Vy Ha <vyha0512@gmail.com>
2022-06-17 19:53:06 +1000

    17/06

--- 3. Build a Blackjack game/18. Add to the sum when newCard is clicked/index.js
+++ 3. Build a Blackjack game/18. Add to the sum when newCard is clicked/index.js
@@ -27,8 +27,9 @@ function startGame() {
 function newCard() {
     console.log("Drawing a new card from the deck!")
     // 1. Create a card variable, and hard code its value to a number (2-11)
-    
+    let card = 6
     // 2. Add the new card to the sum variable
-    
+    sum += card
     // 3. Call startGame()
+    startGame()
 }
