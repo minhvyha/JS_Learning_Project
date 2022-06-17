189a3253ab95742c5d24f08ca81a8e5aac1a10d5
Minh Vy Ha <vyha0512@gmail.com>
2022-06-17 20:01:05 +1000

    17/06

--- 3. Build a Blackjack game/22. Array indexes/index.js
+++ 3. Build a Blackjack game/22. Array indexes/index.js
@@ -13,3 +13,6 @@ let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People coun
 // Frontend developer at Xeneta
 // People counter for Norstat
 // CEO at Scrimba
+console.log(experience[1])
+console.log(experience[2])
+console.log(experience[0])
