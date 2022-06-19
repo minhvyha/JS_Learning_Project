bbb93027ff1ae9f2a437b274567b29c212978e5a
Minh Vy Ha <vyha0512@gmail.com>
2022-06-19 22:15:19 +1000

    19/06

--- 3. Build a Blackjack game/30. Write you first array-based for loop/index.js
+++ 3. Build a Blackjack game/30. Write you first array-based for loop/index.js
@@ -3,4 +3,6 @@ let cards = [7, 3, 9]
 // Create a for loop that logs out all the cards in the array
 // Use cards.length to specify how long the loop should run
 
-
+for (let i = 0; i < cards.length; i += 1){
+    console.log(cards[i])
+}
